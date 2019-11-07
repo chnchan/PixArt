5807b59da84208ac6b7b634c78c365e0fe18af8c
Hin Chan <chnchan@ucdavis.edu>
2019-11-07 01:24:49 -0800

    initial files

---
+++ Pixart/Pixart/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pixart
+//
+//  Created by Hin Chan on 11/7/19.
+//  Copyright © 2019 UC Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
