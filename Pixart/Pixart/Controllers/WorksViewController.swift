6907fef6344e18db2893dc68b34567a23d3bbe85
Ronald Guerra <ronald-mac@Ronalds-MacBook-Air.local>
2019-11-09 21:43:57 -0800

    Table view cell custom class implemented

--- Pixart/Pixart/Controllers/WorksViewController.swift
+++ Pixart/Pixart/Controllers/WorksViewController.swift
@@ -2,16 +2,39 @@
 //  WorksViewController.swift
 //  Pixart
 //
-//  Created by Hin Chan on 11/7/19.
+//  Created by Ronald Guerra on 11/9/19.
 //  Copyright © 2019 UC Davis. All rights reserved.
 //
 
 import UIKit
-import SideMenu
 
 class WorksViewController: UIViewController {
 
+    @IBOutlet weak var worksTableView: UITableView!
+    var works = ["cat", "wolf"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        worksTableView.dataSource = self
+        worksTableView.delegate = self
+       // worksTableView.reloadData()
+        
     }
+    
+
+}
+extension WorksViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return works.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = worksTableView.dequeueReusableCell(withIdentifier: "published_post") as! WorksTableViewCell
+        cell.pixelArtImage.image = UIImage (named: (works[indexPath.row] + ".jpg"))
+        cell.pixelArtName.text = works[indexPath.row]
+        cell.dislikes.text = "10"
+        cell.likes.text = "10"
+        return cell
+    }
+    
 }
