acc4e396b3ab851809a9351c158cf9e286edc3e3
Ronald Guerra <ronald-mac@Ronalds-MacBook-Air.local>
2019-11-11 03:18:21 -0800

    Drawing area implemented. Still working on color picker

--- Pixart/Pixart/Controllers/CanvasViewController.swift
+++ Pixart/Pixart/Controllers/CanvasViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 import SideMenu
+import ColorSlider
 
 class CanvasViewController: UIViewController {
     var root: UIViewController?
     
+    let SLIDER_HIGHT = 15
+    let SLIDER_WIDTH = 300
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+     super.viewDidLoad()
+        let colorSlider = ColorSlider(orientation: .horizontal, previewSide: .top)
+        colorSlider.frame = CGRect( x: Int((view.frame.width)/2) - Int(SLIDER_WIDTH/2), y:  Int(view.frame.height) - 180, width: SLIDER_WIDTH, height: SLIDER_HIGHT)
+             view.addSubview(colorSlider)
     }
+    
+  
 }
---
+++ Pixart/Pixart/Controllers/GridView.swift
@@ -0,0 +1,69 @@
+//
+//  GridView.swift
+//  Pixart
+//
+//  Created by Ronald Guerra on 11/9/19.
+//  Copyright © 2019 UC Davis. All rights reserved.
+//
+
+import UIKit
+
+class GridView: UIView {
+    
+    let CELLS_PER_ROW = 15
+    let CELLS_PER_COLUMN = 25
+    let BORDER_WIDTH: CGFloat = 0.5
+    let BORDER_COLOR = UIColor.black.cgColor
+    let BACKGROUND_COLOR = UIColor.white
+    
+
+    var drawingColor =  UIColor.red
+    var cellWidth: CGFloat = 0
+    var cells = [String: UIView]()
+    
+
+    override func draw(_ rect: CGRect) {
+        
+        self.layer.borderColor = UIColor(red: 255.0/255.0, green: 213.0/255.0, blue: 3.0/255.0, alpha: 1).cgColor
+        self.layer.borderWidth = 3.0
+        
+         // Calculate cell width based on the width of the view
+        cellWidth = self.frame.width / CGFloat(CELLS_PER_ROW)
+        
+        makeCells()
+        self.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture)))
+               
+     }
+
+    @objc func handlePanGesture(gesture: UIPanGestureRecognizer) {
+
+        let location = gesture.location(in: self)
+        let cellWidth = self.frame.width / CGFloat(CELLS_PER_ROW)
+        let i = Int(location.x / cellWidth)
+        let j = Int(location.y / cellWidth)
+        let cellView = cells["\(i)|\(j)"]
+        cellView?.backgroundColor = drawingColor
+    }
+    // This function makes a cell which is a view
+    func makeCells(){
+        
+        //let cellsPerRow:Int = Int(self.frame.height / CGFloat(CELLS_PER_ROW))
+        
+        for j in 0...CELLS_PER_COLUMN {
+            for i in 0...CELLS_PER_ROW - 1{
+                let cellView = UIView()
+                
+                cellView.backgroundColor = BACKGROUND_COLOR
+                cellView.layer.borderWidth = BORDER_WIDTH
+                cellView.layer.borderColor = BORDER_COLOR
+                cellView.frame = CGRect(x: CGFloat(i) * cellWidth, y: CGFloat(j) * cellWidth, width: cellWidth, height: cellWidth)
+
+                self.addSubview(cellView)
+               
+                cells["\(i)|\(j)"] = cellView
+            }
+        }
+    }
+    
+
+}
