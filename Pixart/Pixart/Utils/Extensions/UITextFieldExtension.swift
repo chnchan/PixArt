0a180e70c0cf520baf58180d2b1218460b836114
Hin Chan <chnchan@ucdavis.edu>
2019-11-24 02:45:31 -0800

    Sign in / Sign up redesign.

--- Pixart/Pixart/Utils/Extensions/UITextFieldExtension.swift
+++ Pixart/Pixart/Utils/Extensions/UITextFieldExtension.swift
@@ -10,18 +10,36 @@ import Foundation
 import UIKit
 
 extension UITextField {
-    func setLeftImage(image: String) {
-        let imageView = UIImageView(frame: CGRect(x :0, y: 0, width: 20, height: 20))
-        imageView.image = UIImage(named: image)
-        self.leftView = imageView
+    func setLeftImage(image: String, cornerRadius: CGFloat = 0) {
+        addLeftImage(image: UIImage(named: image), radius: cornerRadius)
+    }
+    
+    func setLeftImage(systemImage: String, cornerRadius: CGFloat = 0) {
+        addLeftImage(image: UIImage(systemName: systemImage), radius: cornerRadius)
+    }
+    
+    func addLeftPadding(width: Int) {
+        let containerView: UIView = UIView(frame:
+        CGRect(x: 0, y: 0, width: width, height: 40))
+        self.leftView = containerView
         self.leftViewMode = .always
     }
     
-    func setLeftImage(systemImage: String) {
-        let imageView = UIImageView(frame: CGRect(x :0, y: 0, width: 20, height: 20))
-        imageView.image = UIImage(systemName: systemImage)
+    private func addLeftImage(image: UIImage?, radius: CGFloat) {
+        let imageView = UIImageView(frame:
+                       CGRect(x: 10, y: 10, width: 20, height: 20))
+        let imageContainerView: UIView = UIView(frame:
+                       CGRect(x: 0, y: 0, width: 40, height: 40))
+        let containerView: UIView = UIView(frame:
+        CGRect(x: 20, y: 0, width: 50, height: 40))
+        imageView.image = image
         imageView.tintColor = UIColor.gray
-        self.leftView = imageView
+        imageView.contentMode = .scaleAspectFit
+        imageContainerView.addSubview(imageView)
+        imageContainerView.backgroundColor = UIColor.white
+        imageContainerView.layer.cornerRadius = radius
+        containerView.addSubview(imageContainerView)
+        self.leftView = containerView
         self.leftViewMode = .always
     }
 }
